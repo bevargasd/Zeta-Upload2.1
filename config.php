84bd6786b5a2f60e3eefb1048bbddaad38675ea9
bevargasd <80842978+bevargasd@users.noreply.github.com>
2021-07-26 02:25:03 -0500

    liteban

--- config.php
+++ config.php
@@ -1,21 +1,68 @@
 <?php
 
-$serverip="play.oryonmc.com";
+//Your minecraft server ip
 
+$serverip="play.pogmc.net";
 
-$serverport="25565";
+//Your minecraft server port
 
+$serverport="19132";
 
-$servername="OryonMC";
+//THIS $servernamedisplay IS ONLY FOR THE DISPLAY PAGE. Leave it empty to display logo
 
+$servernamedisplay="";
 
-$serverdescription="$servername es un gran servidor, entra con: $serverip";
+//This is a subtext below your server's name. Leave it empty to display .
 
+$servernamesubtext="";
+
+//This servername is your server's name. You MUST change this to your servername.
+
+$servername="CoreWeb";
+
+//Servername and server description is used to show this website on google. You can change the description but it isn't nessesary.
+
+$serverdescription="$servername is a new and awesome Minecraft server. You can join with the IP $serverip";
+
+//leave $serverlogo empty to use text. (the file location & name)
 
 $serverlogo="images/logo.png";
 
+//The font you want to use for your text (the file location & name)
 
 $font="fonts/font.otf";
 
+//The background image file location & name. 
+
+$bg="images/bg.png";
+
+//Your forums URL
+
+$forumurl="https://forums.pogmc.net";
+
+//Forums Image URL. A image for the forums card is already included with this website but you can change it if you like.
+
+$forumimageurl="images/forums.png";
+
+//Your Store URL
+
+$storeurl="https://store.pogmc.net";
+
+//Store Image URL. A image for the store card is already included with this website but you can change it if you like.
+
+$storeimageurl="images/store.png";
+
+
+//Your Vote URL
+
+$voteurl="https://vote.pogmc.net";
+
+//Vote Image URL. A image for the vote card is already included with this website but you can change it if you like.
+
+$voteimageurl="images/vote.png";
+
+//Add a "Play" button below the playercounter that launches minecraft and adds your server as an external server. Currently this only works for Minecraft Bedrock.
+
+$show_play_button="true"; //It might be possible to make it join your server in the next update.
 
 ?>
--- index.php
+++ index.php
@@ -1,141 +1,31 @@
-<html>
-    
-    <head>
-        
-          <link rel="stylesheet" href="css/style.css">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-          
-          <?php require "config.php";?>
-          
+<?php
+require_once './inc/page.php';
 
-        <title><?php echo $servername?> - Official Website</title>
-        
-        <meta name="deseription" content="<?php echo $serverdescription?>">
-        
-        <meta property="og:title" content="<?php echo $servername?> - Official Website Portal">
-        
-        <meta property="og:site_nane" content="<?php echo $servername?>">
-        
-        <meta property="og:description" content="<?php echo $serverdescription?>">
-        
-        <meta charset="utf-8">
+$page = new Page("index",false);
 
-        <link rel="icon" type="image/png" href="<?php echo $serverlogo?>">
-        
-    </head>
-    
-    
-    
+if ($page->settings->simple_urls && count($_GET) !== 0) {
+    $target = $page->get_requested_page();
 
-    <body>
-	<header>
-            <div id="particles-js"></div>
-		<video playsinline autoplay muted loop id="bgvid">
- 			 <source src="images/bg.mp4" type="video/webm">
-		</video>
-        </header>
-
-    
-        <?php 
-        
-        if ($serverlogo != null) {
-        
-            ?>
-        <img src="<?php echo $serverlogo?>" class="centered logo"><br><br>
-        
-        <?php 
-    } else {
-    ?>
-    
-        <h1 class="font" id="title"><?php echo $servernamedisplay?></h1>
-        <h1 class="font subtext" id="title"><?php echo $servernamesubtext?></h1>
-    
-    <?php } ?>
-    
-    	<div class="items">
-
-			<a href="sexo.com" class="item store">
-			<div>
-				<img src="images/store.png" alt="Icono de tienda Minecraft" class="img">
-				<p class="subtitle">Compra rango y beneficios en la</p>
-				<p class="title">Tienda</p>
-			</div>
-			</a>
-
-			<a href="youtube.com" class="item forums">
-			<div>
-				<img src="images/forums.png" alt="Minecraft store icon" class="img">
-				<p class="subtitle">Averigua todas las</p>
-				<p class="title">Sanciones</p>
-			</div>
-			</a>
-
-
-			<a href="voto.com" class="item vote">
-			<div>
-				<img src="images/vote.png" alt="Minecraft voting icon" class="img">
-				<p class="subtitle">Ayudanos yendo a</p>
-				<p class="title">Votar</p>
-			</div>
-			</a>
-
-
-		</div>
-    
-    
-    <div class="playercount">
-        
-			<p>&nbsp;Tenemos <span class="sip" data-ip="<?php echo $serverip;?>" data-port="25565">
-			</span> otros jugadores en <span class="ip"><?php echo $serverip?></span>&nbsp;
-			<br>
-		    </div>
-	
-        
-        
-        
-        
-    </body>
-    
-    <script src="https://code.jquery.com/jquery-1.11.2.min.js" type="text/javascript"></script>
-	<script src="js/main.js" type="text/javascript"></script>
-    
-    <style>
-    
-
- 	*{
-	   margin: 0;
-	   paddding: 0;
-	   box-sizing: border-box;	
-	}
-
-	header{
-	   width: 100%;
-	   height: auto;
-
-	
-	}
-
-        video {
-  	position: absolute;
-
-	 left: 0;
-	 top: 0;
-	 width: auto;
-	 height: 150vh;
- 	 z-index: -10;
-	 visibility: visible;
-	 opacity: 0.3;
+    if ($target !== "index" && strlen($target) <= 16 && preg_match("/^[a-z]+$/", $target)) {
+        $local_script = "./${target}.php";
+        if (file_exists($local_script)) {
+            require_once $local_script;
+            return;
+        }
     }
-    @font-face {
-        font-family: title;
-        src: url(<?php echo $font?>);
-    }
-    .btn:hover {
-        cursor:pointer;
-    }
-    </style>
-    
-    
-    
-</html>
+}
+$page->header->print_header();
+
+$page->print_title();
+?>
+<br>
+<div class="container">
+    <div class="jumbotron">
+        <div class="litebans-index litebans-index-main">
+            <h2><?php echo str_replace("{server}", $page->settings->name, $page->t("index.welcome.main")); ?></h2>
+        </div>
+
+        <div class="litebans-index litebans-index-sub"><p><?php echo $page->t("index.welcome.sub"); ?></p></div>
+    </div>
+</div>
+<?php $page->print_footer(false); ?>
