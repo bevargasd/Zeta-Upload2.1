64c8e63920d78689992edb0d6ad8a0df2cc00cd3
bevargasd <80842978+bevargasd@users.noreply.github.com>
2021-07-26 02:05:40 -0500

    aaaa

--- config.php
+++ config.php
@@ -1,75 +1,21 @@
 <?php
-/*
-   ____             __ _         _____ _ _      
-  / ___|___  _ __  / _(_) __ _  |  ___(_) | ___ 
- | |   / _ \| '_ \| |_| |/ _` | | |_  | | |/ _ \
- | |__| (_) | | | |  _| | (_| | |  _| | | |  __/
-  \____\___/|_| |_|_| |_|\__, | |_|   |_|_|\___|
-                         |___/                  
-*/
-return [
-    'theme' => [
-        'title' => 'Website - Portal', # Website title.
-        'logo' => 'images/logo.png', # Path to your servers logo.
-    ],
-    'server' => [
-        'ip' => 'play.lushsurvival.net', # Your Minecraft server ip. (Make sure your query port is open in server.properties)
-        'port' => '25565', # Your Minecraft server port.
-    ],
-    'announcements' => [
-        'enabled' => true, # Enable/disable announcements.
-        'icon' => '<i class="fas fa-scroll"></i>', # Use (https://fontawesome.com/) to get icons.
-            'messages' => [
-                'message' => 'This is a test announcement', # create as many announcements as you'd like just give them a unique name.
-                'message2' => 'change me or add more in ./config.php'
-            ],
-    ],
-    'menu' => [
-        'enabled' => true,
-            'items' => [
-                'forums' => [ # Create as many items as you'd like just give them a unique name.
-                    'url' => '/forums.php',
-                    'icon' => 'fas fa-book-open', # Use (https://fontawesome.com/) to get icons
-                ],
-                'store' => [
-                    'url' => '/store.php',
-                    'icon' => 'fas fa-shopping-cart',
-                ],
-                'vote' => [
-                    'url' => '/vote.php',
-                    'icon' => 'fas fa-poll',
-                ],
-                'vote' => [
-                    'url' => '/vote.php',
-                    'icon' => 'fas fa-poll',
-                ],
-                'bans' => [
-                    'url' => '/bans.php',
-                    'icon' => 'fas fa-gavel',
-                ],
-                'staff' => [
-                    'url' => '/staff.php',
-                    'icon' => 'fas fa-users-cog',
-                ],
-            ], // end of menu items.
-    ],
-    'footer' => [
-        'enabled' => true, # Enable/disable footer.
-            'items' => [
-                'youtube' => [ # Create as many items as you'd like just give them a unique name.
-                    'url' => 'https://youtube.com',
-                    'icon' => '<i class="fab fa-youtube"></i>', # Use (https://fontawesome.com/) to get icons.
-                ],
-                'twitter' => [
-                    'url' => 'https://twitter.com',
-                    'icon' => '<i class="fab fa-twitter"></i>',
-                ],
-                'discord' => [
-                    'url' => 'https://discord.gg',
-                    'icon' => '<i class="fab fa-discord"></i>',
-                ],
-            ], // end of footer items.
-    ]
-];
+
+$serverip="play.oryonmc.com";
+
+
+$serverport="25565";
+
+
+$servername="OryonMC";
+
+
+$serverdescription="$servername es un gran servidor, entra con: $serverip";
+
+
+$serverlogo="images/logo.png";
+
+
+$font="fonts/font.otf";
+
 
 ?>
--- index.php
+++ index.php
@@ -1,50 +1,141 @@
-<?php include('includes/header.php'); ?>
-	<body>
+<html>
+    
+    <head>
+        
+          <link rel="stylesheet" href="css/style.css">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+          
+          <?php require "config.php";?>
+          
+
+        <title><?php echo $servername?> - Official Website</title>
+        
+        <meta name="deseription" content="<?php echo $serverdescription?>">
+        
+        <meta property="og:title" content="<?php echo $servername?> - Official Website Portal">
+        
+        <meta property="og:site_nane" content="<?php echo $servername?>">
+        
+        <meta property="og:description" content="<?php echo $serverdescription?>">
+        
+        <meta charset="utf-8">
+
+        <link rel="icon" type="image/png" href="<?php echo $serverlogo?>">
+        
+    </head>
+    
+    
+    
+
+    <body>
+	<header>
+            <div id="particles-js"></div>
 		<video playsinline autoplay muted loop id="bgvid">
- 			 <source src="images/background.mp4" type="video/webm">
+ 			 <source src="images/bg.mp4" type="video/webm">
 		</video>
-		<?php include('includes/announcements.php'); ?>
-		<div id="wrap">
-			<div class="header">
-				<div class="title">
-					<img src="<?php echo $config['theme']['logo'] ?>"></img>
-				</div>
+        </header>
+
+    
+        <?php 
+        
+        if ($serverlogo != null) {
+        
+            ?>
+        <img src="<?php echo $serverlogo?>" class="centered logo"><br><br>
+        
+        <?php 
+    } else {
+    ?>
+    
+        <h1 class="font" id="title"><?php echo $servernamedisplay?></h1>
+        <h1 class="font subtext" id="title"><?php echo $servernamesubtext?></h1>
+    
+    <?php } ?>
+    
+    	<div class="items">
+
+			<a href="sexo.com" class="item store">
+			<div>
+				<img src="images/store.png" alt="Icono de tienda Minecraft" class="img">
+				<p class="subtitle">Compra rango y beneficios en la</p>
+				<p class="title">Tienda</p>
 			</div>
-			<div class="info">
-				<div>
-					<h2><i class="fas fa-users"></i>
-					<span><?php
-                            	echo $ping['players']['online'];
-                            ?>
-					</span>Players Online</h2>
-					<p>what are you waiting for?</p>
-				</div>
-				<div>
-					<h2><i class="far fa-question-circle"></i> Server Information</h2>
-					<p id="content-1">
-						Status: <span>
-							<?php
-								if ($ping) echo 'Online';
-                            	else echo 'Offline';
-							?>
-						</span>
-					</p>
-					<p id="content-2">
-						Version: <span>
-							<?php
-								echo $ping['version']['name'];
-							?>		
-						</span>
-					</p>
-				</div>
-				<div>
-					<button id="copy" data-hover="Click to Copy IP" data-active="active" data-clipboard-text="<?php echo $config['server']['ip']?>" type="button">
-					<span><?php echo $config['server']['ip']?></span></button>
-				</div>
+			</a>
+
+			<a href="youtube.com" class="item forums">
+			<div>
+				<img src="images/forums.png" alt="Minecraft store icon" class="img">
+				<p class="subtitle">Averigua todas las</p>
+				<p class="title">Sanciones</p>
 			</div>
-			<?php include'includes/menu.php'?>
-			<?php include'includes/footer.php'?>
+			</a>
+
+
+			<a href="voto.com" class="item vote">
+			<div>
+				<img src="images/vote.png" alt="Minecraft voting icon" class="img">
+				<p class="subtitle">Ayudanos yendo a</p>
+				<p class="title">Votar</p>
+			</div>
+			</a>
+
+
 		</div>
-		<script type="text/javascript" src="javascript/functions.js"></script>
-	</body>
+    
+    
+    <div class="playercount">
+        
+			<p>&nbsp;Tenemos <span class="sip" data-ip="<?php echo $serverip;?>" data-port="25565">
+			</span> otros jugadores en <span class="ip"><?php echo $serverip?></span>&nbsp;
+			<br>
+		    </div>
+	
+        
+        
+        
+        
+    </body>
+    
+    <script src="https://code.jquery.com/jquery-1.11.2.min.js" type="text/javascript"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+    
+    <style>
+    
+
+ 	*{
+	   margin: 0;
+	   paddding: 0;
+	   box-sizing: border-box;	
+	}
+
+	header{
+	   width: 100%;
+	   height: auto;
+
+	
+	}
+
+        video {
+  	position: absolute;
+
+	 left: 0;
+	 top: 0;
+	 width: auto;
+	 height: 150vh;
+ 	 z-index: -10;
+	 visibility: visible;
+	 opacity: 0.3;
+    }
+    @font-face {
+        font-family: title;
+        src: url(<?php echo $font?>);
+    }
+    .btn:hover {
+        cursor:pointer;
+    }
+    </style>
+    
+    
+    
 </html>
